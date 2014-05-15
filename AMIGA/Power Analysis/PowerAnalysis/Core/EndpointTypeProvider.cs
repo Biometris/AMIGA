dba30132e0d95b23e3e708c23a554aa4fd46c05b
kruisselbrink <johannes.kruisselbrink@wur.nl>
2014-05-15 06:58:38 +0000

    Update solution to vs2012.
    git-svn-id: svn://biometris-nas.wurnet.nl/AMIGA@9 3ae013c2-4042-2242-b34d-985be216b4aa

--- AMIGA/Power Analysis/PowerAnalysis/Core/EndpointTypeProvider.cs
+++ AMIGA/Power Analysis/PowerAnalysis/Core/EndpointTypeProvider.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AmigaPowerAnalysis.Core {
     public sealed class EndpointTypeProvider {
