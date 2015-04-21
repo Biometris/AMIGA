89b8efc590557ca838b416fde35511b151302b43
kruisselbrink <johannes.kruisselbrink@wur.nl>
2015-04-21 14:55:17 +0000

    Added Biometris common library to solution.
    git-svn-id: svn://biometris-nas.wurnet.nl/AMIGA@295 3ae013c2-4042-2242-b34d-985be216b4aa

---
+++ AmigaPowerAnalysis/Biometris/ApplicationUtilities/ApplicationUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace Biometris.ApplicationUtilities {
+
+    public static class ApplicationUtils {
+
+        /// <summary>
+        /// Determines the application's temp path, creates it if it doesn't exist
+        /// and returns it.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetApplicationTempPath() {
+            var tempdir = Path.Combine(Path.GetTempPath(), Application.ProductName);
+            if (!Directory.Exists(tempdir)) {
+                Directory.CreateDirectory(tempdir);
+            }
+            return tempdir;
+        }
+
+        /// <summary>
+        /// Clears the application's temp path.
+        /// </summary>
+        public static void ClearApplicationTempPath() {
+            var tempPathDirectoryInfo = new DirectoryInfo(GetApplicationTempPath());
+            foreach (FileInfo file in tempPathDirectoryInfo.GetFiles()) {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in tempPathDirectoryInfo.GetDirectories()) {
+                dir.Delete(true);
+            }
+        }
+    }
+}
---
+++ AmigaPowerAnalysis/Biometris/R/REngines/RDotNet/LoggingRDotNetEngine.cs
@@ -0,0 +1,34 @@
+﻿using Biometris.Logger;
+
+namespace Biometris.R.REngines {
+
+    /// <summary>
+    /// A dot net engine instance that includes a command logger for logging
+    /// the R commands.
+    /// </summary>
+    public sealed class LoggingRDotNetEngine : RDotNetEngine {
+
+        /// <summary>
+        /// R commands logger.
+        /// </summary>
+        private ILogger _commandLogger = null;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public LoggingRDotNetEngine(ILogger logger = null) : base() {
+            _commandLogger = logger;
+        }
+
+        /// <summary>
+        /// Evaluates the given R command in the R environment.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        protected override void LogCommand(string command) {
+            if (_commandLogger != null) {
+                _commandLogger.Log(command);
+            }
+        }
+    }
+}
