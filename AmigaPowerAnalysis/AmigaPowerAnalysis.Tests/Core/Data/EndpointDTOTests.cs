b86992e0502ad7247e0b5459ee80261912a3c004
kruisselbrink <johannes.kruisselbrink@wur.nl>
2015-07-09 04:13:39 +0000

    Added endpoint and factor dto tests.
    git-svn-id: svn://biometris-nas.wurnet.nl/AMIGA@321 3ae013c2-4042-2242-b34d-985be216b4aa

---
+++ AmigaPowerAnalysis/AmigaPowerAnalysis.Tests/Core/Data/EndpointDTOTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using AmigaPowerAnalysis.Core;
+using AmigaPowerAnalysis.Core.Data;
+using AmigaPowerAnalysis.Core.DataReaders;
+using Biometris.Persistence;
+using Biometris.ExtensionMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biometris.Statistics.Measurements;
+using Biometris.Statistics.Distributions;
+using System.Collections.Generic;
+
+namespace AmigaPowerAnalysis.Tests.Core {
+    [TestClass]
+    public class EndpointDTOTests {
+
+        [TestMethod]
+        public void EndpointGroupDTO_TestSingle() {
+            var filename = @"SingleGroup.csv";
+            var original = new EndpointType("Non-Target insects counts", true, MeasurementType.Count, 0, 0.5, 2, 10, 100, DistributionType.PowerLaw, 1.7);
+            var dtoOriginal = original.ToDTO();
+            CsvWriter.WriteToCsvFile(filename, ",", new List<EndpointGroupDTO>() { dtoOriginal });
+            var outputFileReader = new DTODataFileReader();
+            var record = outputFileReader.ReadGroups(filename).Single().ToEndpointGroup();
+            Assert.IsTrue(ObjectComparisonExtensions.PublicInstancePropertiesEqual(original, record));
+        }
+
+        [TestMethod]
+        public void EndpointGroupDTO_TestMultiple() {
+            var filename = @"DefaultGroups.csv";
+            var defaultGroups = EndpointTypeProvider.NewProjectDefaultEndpointTypes();
+            CsvWriter.WriteToCsvFile(filename, ",", defaultGroups.Select(r => r.ToDTO()));
+            var outputFileReader = new DTODataFileReader();
+            var records = outputFileReader.ReadGroups(filename).Select(r => r.ToEndpointGroup()).ToList();
+            var zips = defaultGroups.Zip(defaultGroups, (d, r) => new { d, r });
+            Assert.AreEqual(defaultGroups.Count, records.Count);
+            foreach (var zip in zips) {
+                Assert.IsTrue(ObjectComparisonExtensions.PublicInstancePropertiesEqual(zip.r, zip.d));
+            }
+        }
+    }
+}
---
+++ AmigaPowerAnalysis/AmigaPowerAnalysis.Tests/Core/Data/FactorDTOTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using AmigaPowerAnalysis.Core;
+using AmigaPowerAnalysis.Core.Data;
+using AmigaPowerAnalysis.Core.DataReaders;
+using Biometris.Persistence;
+using Biometris.ExtensionMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biometris.Statistics.Measurements;
+using Biometris.Statistics.Distributions;
+using System.Collections.Generic;
+
+namespace AmigaPowerAnalysis.Tests.Core {
+    [TestClass]
+    public class FactorDTOTests {
+
+        [TestMethod]
+        public void FactorDTO_TestSingle() {
+            var filename = @"SingleFactor.csv";
+            var original = new Factor() {
+                Name = "Single Factor",
+                IsInteractionWithVariety = true,
+                ExperimentUnitType = ExperimentUnitType.SubPlot
+            };
+            var dtoOriginal = original.ToDTO();
+            CsvWriter.WriteToCsvFile(filename, ",", new List<FactorDTO>() { dtoOriginal });
+            var outputFileReader = new DTODataFileReader();
+            var dtoRecord = outputFileReader.ReadFactors(filename).Single();
+            var record = dtoRecord.ToFactor();
+            Assert.IsTrue(ObjectComparisonExtensions.PublicInstancePropertiesEqual(dtoOriginal, dtoRecord));
+            Assert.IsTrue(ObjectComparisonExtensions.PublicInstancePropertiesEqual(original, record));
+        }
+
+        [TestMethod]
+        public void FactorDTO_TestSingleVarietyFactor() {
+            var filename = @"SingleVarietyFactor.csv";
+            var original = new VarietyFactor();
+            var dtoOriginal = original.ToDTO();
+            CsvWriter.WriteToCsvFile(filename, ",", new List<FactorDTO>() { dtoOriginal });
+            var outputFileReader = new DTODataFileReader();
+            var record = outputFileReader.ReadFactors(filename).Single().ToFactor();
+            Assert.IsTrue(ObjectComparisonExtensions.PublicInstancePropertiesEqual(original, record));
+        }
+
+        [TestMethod]
+        public void FactorDTO_TestMultiple() {
+            var filename = @"MultipleFactors.csv";
+
+        }
+    }
+}
