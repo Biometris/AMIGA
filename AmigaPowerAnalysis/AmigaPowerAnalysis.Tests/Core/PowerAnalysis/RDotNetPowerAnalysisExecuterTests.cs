4a67ab41adbf185202489c6b2cd2ea2f88e794fe
kruisselbrink <johannes.kruisselbrink@wur.nl>
2015-08-21 11:59:04 +0000

    Added mock project creator for tests.
    git-svn-id: svn://biometris-nas.wurnet.nl/AMIGA@347 3ae013c2-4042-2242-b34d-985be216b4aa

--- AmigaPowerAnalysis/AmigaPowerAnalysis.Tests/Core/PowerAnalysis/RDotNetPowerAnalysisExecuterTests.cs
+++ AmigaPowerAnalysis/AmigaPowerAnalysis.Tests/Core/PowerAnalysis/RDotNetPowerAnalysisExecuterTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using AmigaPowerAnalysis.Core;
 using AmigaPowerAnalysis.Core.DataAnalysis.AnalysisModels;
 using AmigaPowerAnalysis.Core.PowerAnalysis;
+using AmigaPowerAnalysis.Tests.Mocks.Projects;
 using Biometris.Persistence;
 using Biometris.ProgressReporting;
 using Biometris.Statistics.Distributions;
@@ -14,29 +15,9 @@ namespace AmigaPowerAnalysis.Tests.Core {
     [TestClass]
     public class RDotNetPowerAnalysisExecuterTests {
 
-        private static Project createMockProject() {
-            var project = new Project();
-            var endpointType = new EndpointType("Yield", true, MeasurementType.Count, 0, 0.8, 1.2, 80, 0.5, DistributionType.OverdispersedPoisson, 0);
-            var endpoint = new Endpoint("Endpoint", endpointType);
-            project.AddEndpoint(endpoint);
-
-            var factorF1 = new Factor("F1", 3, false);
-            project.AddFactor(factorF1);
-
-            var factorF2 = new Factor("F2", 3, false);
-            project.AddFactor(factorF2);
-
-            project.PowerCalculationSettings.NumberOfRatios = 2;
-            project.PowerCalculationSettings.NumberOfReplications = new List<int>() { 2, 4 };
-            project.PowerCalculationSettings.NumberOfSimulatedDataSets = 10;
-            project.PowerCalculationSettings.SelectedAnalysisMethodTypes = AnalysisMethodType.LogNormal | AnalysisMethodType.SquareRoot | AnalysisMethodType.OverdispersedPoisson | AnalysisMethodType.NegativeBinomial;
-
-            return project;
-        }
-
         [TestMethod]
         public void RDotNetPowerAnalysisExecuter_TestRunAnalysis1() {
-            var project = createMockProject();
+            var project = MockProjectsCreator.MockProject1();
 
             var comparison = project.GetComparisons().First();
             var inputGenerator = new PowerAnalysisInputGenerator();
---
+++ AmigaPowerAnalysis/AmigaPowerAnalysis.Tests/Mocks/Projects/MockProjectsCreator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AmigaPowerAnalysis.Core;
+using AmigaPowerAnalysis.Core.DataAnalysis.AnalysisModels;
+using Biometris.Statistics.Distributions;
+using Biometris.Statistics.Measurements;
+
+namespace AmigaPowerAnalysis.Tests.Mocks.Projects {
+    public static class MockProjectsCreator {
+
+        public static Project MockProject1() {
+            var project = new Project();
+            var endpointType = new EndpointType("Yield", true, MeasurementType.Count, 0, 0.8, 1.2, 80, 0.5, DistributionType.OverdispersedPoisson, 0);
+            var endpoint = new Endpoint("Endpoint", endpointType);
+            project.AddEndpoint(endpoint);
+
+            var factorF1 = new Factor("F1", 3, false);
+            project.AddFactor(factorF1);
+
+            var factorF2 = new Factor("F2", 3, false);
+            project.AddFactor(factorF2);
+
+            project.PowerCalculationSettings.NumberOfRatios = 2;
+            project.PowerCalculationSettings.NumberOfReplications = new List<int>() { 2, 4 };
+            project.PowerCalculationSettings.NumberOfSimulatedDataSets = 10;
+            project.PowerCalculationSettings.SelectedAnalysisMethodTypes = AnalysisMethodType.LogNormal | AnalysisMethodType.SquareRoot | AnalysisMethodType.OverdispersedPoisson | AnalysisMethodType.NegativeBinomial;
+
+            return project;
+        }
+    }
+}
