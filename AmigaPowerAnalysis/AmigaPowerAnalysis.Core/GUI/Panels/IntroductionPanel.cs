6805b2b6837802386f3dbd8235e56049d7e6238b
vandervoet <hilko.vandervoet@wur.nl>
2015-10-09 09:51:12 +0000

    tekst
    git-svn-id: svn://biometris-nas.wurnet.nl/AMIGA@549 3ae013c2-4042-2242-b34d-985be216b4aa

--- AmigaPowerAnalysis/AmigaPowerAnalysis.Core/GUI/Panels/IntroductionPanel.cs
+++ AmigaPowerAnalysis/AmigaPowerAnalysis.Core/GUI/Panels/IntroductionPanel.cs
@@ -9,7 +9,7 @@ namespace AmigaPowerAnalysis.GUI {
         public IntroductionPanel() {
             InitializeComponent();
             Name = "Introduction";
-            Description = "Welcome to Amiga Power Analysis for environmental risk assessment (ERA) using field trials.\r\nYou can calculate the necessary replication for assessing differences and equivalences between a test and a comparator plant variety under different data models for count and continuous data.\r\nThe tool allows to specify the experimental design, additional factors in the experiment, and the method of statistical analysis that will be used.\r\nStart by opening an existing file or creating a new file (File menu).\r\nNote: The current implementation requires that the statistical system R is available (http://www.r-project.org/).";
+            Description = "Welcome to APA, the Amiga Power Analysis for environmental risk assessment (ERA) using field trials.\r\nYou can calculate the necessary replication for assessing differences and equivalences between a test and a comparator plant variety under different data models for count and continuous data.\r\nAPA allows to specify the experimental design, additional factors in the experiment, and the method of statistical analysis that will be used.\r\nStart by opening an existing file or creating a new file (File menu).\r\nNote: The current implementation requires that the statistical system R is available (http://www.r-project.org/).";
         }
 
         public event EventHandler TabVisibilitiesChanged;
