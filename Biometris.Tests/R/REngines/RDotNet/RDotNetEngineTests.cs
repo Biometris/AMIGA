3613951c4c73b7c565367601a293de5ce797ce0b
Johannes Kruisselbrink <johannes.kruisselbrink@wur.nl>
2023-02-28 08:36:41 +0100

    test: add R.Net engine test

---
+++ Biometris.Tests/R/REngines/RDotNet/RDotNetEngineTests.cs
@@ -0,0 +1,15 @@
+﻿using Biometris.R.REngines;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Biometris.Tests.R.REngines.RDotNet {
+    [TestClass]
+    public class RDotNetEngineTests {
+        [TestMethod]
+        public void RDotNetEngine_TestLoadLibrary() {
+            using (var rEngine = new RDotNetEngine()) {
+                rEngine.LoadLibrary("MASS");
+                rEngine.LoadLibrary("lsmeans");
+            }
+        }
+    }
+}
